cc126ffee84d334ebea3bffe9f60f03e8a48e8bf
asillers <asillers@ucdavis.edu>
2026-08-10 11:27:49 -0700

    script updates

--- seqkit2.sh
+++ seqkit2.sh
@@ -10,12 +10,11 @@
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=12:00:00
+#SBATCH --array=1-3
 
 module load conda
 conda activate seqkit
 
-seqkit stats -a Hifiasm_out/E1G0*ctg.fasta > SeqStats/E1_hifiasm.stats
+prefix=$(ls Hifiasm_out/*ctg.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F. '{print $1}')
 
-seqkit stats -a Hifiasm_out/E2G0*ctg.fasta > SeqStats/E2_hifiasm.stats
-
-seqkit stats -a Hifiasm_out/G1G0*ctg.fasta > SeqStats/G1_hifiasm.stats
+seqkit stats -a Hifiasm_out/${prefix}*ctg.fasta > SeqStats/${prefix}_hifiasm.stats
