79a9da027c953152cdbc732565a69185758412b5
asillers <asillers@ucdavis.edu>
2026-08-11 14:20:58 -0700

    script added

---
+++ seqkit3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --job-name=seqkit
+#SBATCH --ntasks=2 # Number of cores
+#SBATCH --nodes=1 # Ensure that all cores are on one machine
+#SBATCH --mem=30G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --partition=bmh # Partition to submit to
+#SBATCH --output=seqkit3.out # File to which STDOUT will be written
+#SBATCH --error=seqkit3.err # File to which STDERR will be written
+#SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
+#SBATCH --time=12:00:00
+#SBATCH --array=1-3
+
+module load conda
+conda activate seqkit
+
+prefix=$(ls *.hapbridge.vcf.gz | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F. '{print $1}')
+
+seqkit stats -a ragtag_output/${prefix}hap1/ragtag.scaffold.fasta > SeqStats/${prefix}_hap1.stats
+
+seqkit stats -a ragtag_output/${prefix}hap2/ragtag.scaffold.fasta > SeqStats/${prefix}_hap2.stats
